76a116d3ee046b06a41ba2cd38030f2f9c4ee16e
Chris S <chris@soundshare.co.uk>
2020-07-09 12:18:32 +0100

    docker-compose run test

--- x.sh
+++ x.sh
@@ -1,2 +1,13 @@
 echo "testing 123"
 docker-compose --version
+
+docker-compose up -d 
+
+
+n=0
+until [ $n -ge 24 ]
+do
+  curl $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker-compose ps -q))
+  n=$[$n+1]
+  sleep 5
+done
