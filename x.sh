6622f0c764343056110d54c079bb7bbf150c20f6
Chris S <chris@soundshare.co.uk>
2020-07-09 12:21:14 +0100

    docker-compose down

--- x.sh
+++ x.sh
@@ -8,6 +8,8 @@ n=0
 until [ $n -ge 24 ]
 do
   curl $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker-compose ps -q))
-  n=$[$n+1]
+  n=$[$n+1] && break
   sleep 5
 done
+
+docker-compose down -v
